83bc9bde0aa470a775e8a2e389bd7a469df5a763
mj-brown <brown8mj5@gmail.com>
2023-10-16 16:44:31 -1000

    added a dialogue box and changed the html.

--- unit01/block07/script.js
+++ unit01/block07/script.js
@@ -9,13 +9,10 @@ let numberTwo = 5 * 8; //Using multiplication 5*8=40.
 //numberThree = 39 with subtraction.
 let numberThree = 50 - 11; //Using subtraction 50-11=39.
 
-//Assign the message to showup in the browser to the element with the id of "message".
-let messageForYou = document.getElementById("message");
-
 //Create a string stating: 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
 
 //String will include the three variables also.
 let message = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${numberOne} - ${numberTwo} - ${numberThree}.`;
 
-//Calls the message into the browser.
-messageForYou.innerHTML = message;
+//Calls the message into a dialogue box.
+window.alert(message);
